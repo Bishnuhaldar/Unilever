7fbe006e102ec8abc594175ce6936d1ad3087b22
Bishnuhaldar <109227807+Bishnuhaldar@users.noreply.github.com>
2024-11-13 16:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,20 +191,20 @@ country_query = f"""
 
 # Slider to set limit of result
 
-# Sidebar for chart type selection
-# st.sidebar.subheader("Chart Type")
-# chart_types = {
-#     "Bar Chart": st.sidebar.checkbox("Bar Chart"),
-#     "Pie Chart": st.sidebar.checkbox("Pie Chart"),
-#     "Line Chart": st.sidebar.checkbox("Line Chart"),
-#     "Histogram": st.sidebar.checkbox("Histogram"),
-#     "Radar Chart": st.sidebar.checkbox("Radar Chart")
-# }
+#Sidebar for chart type selection
+st.sidebar.subheader("Chart Type")
+chart_types = {
+    "Bar Chart": st.sidebar.checkbox("Bar Chart"),
+    "Pie Chart": st.sidebar.checkbox("Pie Chart"),
+    "Line Chart": st.sidebar.checkbox("Line Chart"),
+    "Histogram": st.sidebar.checkbox("Histogram"),
+    "Radar Chart": st.sidebar.checkbox("Radar Chart")
+}
 
 # st.sidebar.radio('',['Finance','Supply chain',
 # 'Revenue growth','IT ops', 'SDLC'])
 
-# limit = st.sidebar.slider('Limit Of Output', 0, 100, 10)
+limit = st.sidebar.slider('Limit Of Output', 0, 100, 10)
 
 questions=['what are the potential options to reduce churn by 2%','what are the options to bring down marketing costs','key options to increase customer satisfaction','how to increase customer acquisition by 20%','how to reduce acquisition cost','how to increase retention','increase market share & profitability','what was the total sales revenue for the last quarter','can you segment our customers based on their purchase frequency','what are our most frequent purchasers buying that our least frequent purchasers aren’t',
            'are there any other distinguishing factors between the purchase frequency segments','what is the overall sentiment of our customer reviews','can you summarize the key points of feedback from our customer reviews','is there any missing data or other questions that we should be asking for customer feedback on that could improve analysis',
